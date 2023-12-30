f8956bf55828e4d3299ff4783cbd77da21ac4810
Michel Hilgemberg <michelhilg@gmail.com>
2023-12-30 14:43:31 -0300

    Add swagger doc using restx library

--- app/views.py
+++ app/views.py
@@ -1,57 +1,62 @@
 from flask import Blueprint, jsonify, request, current_app
+from flask_restx import Resource, Api
 import datetime
 import pytz
 from database.database import DatabaseManager
 
 app_blueprint = Blueprint('app', __name__)
 
-@app_blueprint.route('/predict', methods=['POST'])
-def predict():
-    """
-    Handle POST requests to the '/predict' endpoint.
+api = Api(app_blueprint, version='1.0', title='Model Serving API', description='API for AI model predictions.')
+ns = api.namespace('prediction', description='Prediction related operations')
 
-    Returns:
-    - JSON: A JSON response with prediction results or error messages.
-    """
-    
-    model = current_app.model
-    desired_timezone = current_app.desired_timezone
-    logger = current_app.logger
-    database_path = current_app.database_path
+@ns.route('/results')
+class PredictResource(Resource):
+    def post(self):
+        """
+        Handle POST requests to the '/prediction/results' endpoint.
 
-    db_manager = DatabaseManager(database_path)
+        Returns:
+        - A JSON response with prediction results or error messages.
+        """
+        
+        model = current_app.model
+        desired_timezone = current_app.desired_timezone
+        logger = current_app.logger
+        database_path = current_app.database_path
 
-    # Autoincrement in SQLite for the request id
-    with db_manager.get_db() as conn:
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO identificadores (id) VALUES (NULL)')
-        conn.commit()
-        last_id = cursor.lastrowid  
+        db_manager = DatabaseManager(database_path)
 
-    current_time = datetime.datetime.now(pytz.timezone(desired_timezone)).isoformat()
+        # Autoincrement in SQLite for the request id
+        with db_manager.get_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO identificadores (id) VALUES (NULL)')
+            conn.commit()
+            last_id = cursor.lastrowid  
 
-    try:
-        feature_1 = float(request.json['feature_1'])
-        feature_2 = float(request.json['feature_2'])
-        prediction = model.predict([[feature_1, feature_2]])[0]
+        current_time = datetime.datetime.now(pytz.timezone(desired_timezone)).isoformat()
 
-        response = {
-            "data": current_time,
-            "predicao": round(prediction, 5),
-            "id": last_id
-        }
+        try:
+            feature_1 = float(request.json['feature_1'])
+            feature_2 = float(request.json['feature_2'])
+            prediction = model.predict([[feature_1, feature_2]])[0]
 
-        logger.info(f"status: 200, id: {last_id}, feature_1: {feature_1}, feature_2: {feature_2}, predição: {prediction}")
-        return jsonify(response)
+            response = {
+                "data": current_time,
+                "predicao": round(prediction, 5),
+                "id": last_id
+            }
 
-    except ValueError as ve:
-        # Handling value errors caused by invalid input data
-        error_message = f"Invalid input data: {str(ve)}"
-        logger.error(f"status: 400, id: {last_id}, mensagem de erro: {error_message}")
-        return jsonify({"error": error_message}), 400
+            logger.info(f"status: 200, id: {last_id}, feature_1: {feature_1}, feature_2: {feature_2}, predição: {prediction}")
+            return jsonify(response)
 
-    except Exception as e:
-        # Handling unexpected errors
-        error_message = f"Unexpected error: {str(e)}"
-        logger.error(f"status: 500, id: {last_id}, mensagem de erro: {error_message}")
-        return jsonify({"error": error_message}), 500
+        except ValueError as ve:
+            # Handling value errors caused by invalid input data
+            error_message = f"Invalid input data: {str(ve)}"
+            logger.error(f"status: 400, id: {last_id}, mensagem de erro: {error_message}")
+            return jsonify({"error": error_message}), 400
+
+        except Exception as e:
+            # Handling unexpected errors
+            error_message = f"Unexpected error: {str(e)}"
+            logger.error(f"status: 500, id: {last_id}, mensagem de erro: {error_message}")
+            return jsonify({"error": error_message}), 500
